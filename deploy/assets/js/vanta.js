f313bb5b2a0039ebb25dab0c859528561b96a613
Sniij <120099321+Sniij@users.noreply.github.com>
2023-12-20 19:35:50 +0900

    [edit] 배경 vanta 오픈소스 활용하여 변경

---
+++ deploy/assets/js/vanta.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    VANTA.BIRDS({
+        el: "#birds",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x0,
+        color1: 0xf00ff,
+        color2: 0xf50000,
+        birdSize: 1.10,
+        cohesion: 28.00,
+        quantity: 4.50,
+        backgroundAlpha: 1
+    })
+}
+
