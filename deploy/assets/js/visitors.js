b9f7fe6b92b9804c92a714ddc981992996349ed4
Sniij <120099321+Sniij@users.noreply.github.com>
2024-01-02 20:44:44 +0900

    [add] lambda + api gateway를 활용한 방문자 수 추가

---
+++ deploy/assets/js/visitors.js
@@ -0,0 +1,29 @@
+let url = 'https://portfolio.chomanki.com/visitors/portfolio-ga4';
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = yyyy + mm + dd;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let totalVisitors = 0;
+        let todayVisitors = 0;
+
+        data.rows.forEach(row => {
+            let visitorCount = parseInt(row.metricValues[0].value);
+            totalVisitors += visitorCount;
+
+            if (row.dimensionValues[1].value === today) {
+                todayVisitors += visitorCount;
+            }
+        });
+
+        // HTML 요소에 값을 출력
+        document.getElementById('totalVisitors').innerText = totalVisitors;
+        document.getElementById('todayVisitors').innerText = todayVisitors;
+    })
+    .catch(error => console.error('Error:', error));
